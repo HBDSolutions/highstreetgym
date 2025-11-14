0975ba656a4ee42d9716802b7403f57e401411d3
Graeme Lowe <graeme.lowe@happinessbydesign.co.nz>
2025-11-14 15:13:11 +1300

    Public styling updates in blog and class pages

--- .history/views/partials/blog/blog_login_20251030232608.php
+++ .history/views/partials/blog/blog_login_20251030232608.php
@@ -1,5 +1,5 @@
 <!-- Blog Login Prompt Partial -->
-<p class="alert" style="background-color: var(--hsg-muted); border-color: var(--hsg-muted);">
+<p class="alert alert-info">
     <a href="/highstreetgym/controllers/auth/login_controller.php">Login</a> or 
     <a href="/highstreetgym/controllers/auth/register_controller.php">Register</a> 
     to share your fitness journey with fellow members.
